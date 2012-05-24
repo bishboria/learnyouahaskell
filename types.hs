8bfa79e448f6eb345e2d96d0788fcc58081c79cc
Stuart Gale <bishboria@gmail.com>
2012-05-24 23:32:18 +0100

    .

--- types.hs
+++ types.hs
@@ -4,3 +4,11 @@ True :: Bool
 "HELLO!" :: [Char]
 (True, 'a') :: (Bool, Char)
 4 == 5 :: Bool
+
+-- function definiton with type declaration
+removeNonUpperCase :: [Char] -> [Char] -- Takes one parameter, a [Char]
+				       -- Returns [Char]
+removeNonUpperCase st = [c | c  <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int -- Takes 3 Ints, Returns Int
+addThree x y z = x + y + z
