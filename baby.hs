bc7fb5dc4ca608b1b4d0a53b387e2b664727780e
Stuart Gale <bishboria@gmail.com>
2012-05-24 22:01:35 +0100

    .

--- baby.hs
+++ baby.hs
@@ -71,3 +71,5 @@ comprehensionMultiplesTwoFilterMultiples = [x*2 | x <- [1..10], x*2 >= 12]
 comprehensionFilterMod = [x | x <- [50..100], x `mod` 7 == 3]
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+comprenhensionFilterNot = [x | x <- [10..20], x /= 13, x /= 15, x /19]
