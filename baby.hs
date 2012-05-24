9e4e97fe2370f187599156f80220c98f0f2b2bbd
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:53:59 +0100

    .

--- baby.hs
+++ baby.hs
@@ -34,7 +34,7 @@ startList = [5,4,3,2,1]
 middleOfList = init (tail startList)
 middleOfList' = tail (init startList)
 
-lengthOfList = length [5,4,3,2,1]
+lengthList = length [5,4,3,2,1]
 
 isNull = null [1,2,3]
 
