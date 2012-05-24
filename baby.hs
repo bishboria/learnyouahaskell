2b63e4650230bb6db71fec7e9de13c755b9a85f9
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:51:51 +0100

    .

--- baby.hs
+++ baby.hs
@@ -9,11 +9,11 @@ doubleSmallNumber' x = (if x > 100 then x else x*2)
 conanO'Brien = "It's a-me, Conan O'Brien!"
 lostNumbers = [4,8,15,16,23,42]
 
-listConcatenate = [1,2,3,4] ++ [9,10,11,12] -- [a] ++ [a]
-stringConcatenate = "hello" ++ " " ++ "world"
-stringConcatenate' = ['w','0'] ++ ['0','t']
+concatenateLists = [1,2,3,4] ++ [9,10,11,12] -- [a] ++ [a]
+concatenateStrings = "hello" ++ " " ++ "world"
+concatenateStrings' = ['w','0'] ++ ['0','t']
 
-stringCons = 'A':" SMALL CAT" -- a:[a]
+consStrings = 'A':" SMALL CAT" -- a:[a]
 consNumbers = 5:[1,2,3,4,5]
 consNumbers' = 5:1:2:3:4:5:[]
 
