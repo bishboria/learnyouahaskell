317435577e6cc9af10d2adb2d07d2a775bb43185
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:45:22 +0100

    .

--- baby.hs
+++ baby.hs
@@ -67,3 +67,4 @@ replicatedElement = replicate 3 10 -- 3 `replicate` 10
 dodgyFloatRange = [0.1, 0.3 .. 1]
 
 multiplesOfTwoComprehension = [x*2 | x <- [1..10]]
+multiplesOfTwoComprehensionGreaterThanTwelve = [x*2 | x <- [1..10], x*2 >= 12]
