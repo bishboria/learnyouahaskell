adf9c29bd6868166a695c905f82f80eed416c535
Stuart Gale <bishboria@gmail.com>
2012-05-24 19:18:43 +0100

    .

--- baby.hs
+++ baby.hs
@@ -12,13 +12,13 @@ conanO'Brien = "It's a-me, Conan O'Brien!"
 
 lostNumbers = [4,8,15,16,23,42]
 
-concatenateLists = [1,2,3,4] ++ [9,10,11,12]
+concatenateLists = [1,2,3,4] ++ [9,10,11,12] -- [a] ++ [a]
 
 concatenateStrings = "hello" ++ " " ++ "world"
 
 concatenateStrings' = ['w','0'] ++ ['0','t']
 
-consStrings = 'A':" SMALL CAT"
+consStrings = 'A':" SMALL CAT" -- a:[a]
 
 consNumbers = 5:[1,2,3,4,5]
 
