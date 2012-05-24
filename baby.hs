8891b1f045537baec59972d4fdcc558f25c01394
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:55:24 +0100

    .

--- baby.hs
+++ baby.hs
@@ -60,7 +60,7 @@ rangeDecreasing = [20,19..1]
 
 rangeSteppedInfiniteTake = take 10 [13,26..]
 
-cycledList = take 10 (cycle [1,2,3])
+cycleListTake = take 10 (cycle [1,2,3])
 repeatedElement = take 10 (repeat 5)
 replicatedElement = replicate 3 10 -- 3 `replicate` 10
 
