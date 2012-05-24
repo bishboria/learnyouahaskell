accebead1ed5d015c82798a0dfca076f3e66c6dc
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:42:16 +0100

    .

--- baby.hs
+++ baby.hs
@@ -61,9 +61,10 @@ decreasingRange = [20,19..1]
 takeFromSteppedInfiniteRange = take 10 [13,26..]
 
 cycledList = take 10 (cycle [1,2,3])
-
 repeatedElement = take 10 (repeat 5)
-
 replicatedElement = replicate 3 10 -- 3 `replicate` 10
 
 dodgyFloatRange = [0.1, 0.3 .. 1]
+
+
+
