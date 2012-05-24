c24cd53b2bdcbf546f7528ae888591594736f1d5
Stuart Gale <bishboria@gmail.com>
2012-05-24 21:50:10 +0100

    .

--- baby.hs
+++ baby.hs
@@ -67,5 +67,5 @@ replicatedElement = replicate 3 10 -- 3 `replicate` 10
 dodgyFloatRange = [0.1, 0.3 .. 1]
 
 multiplesOfTwoComprehension = [x*2 | x <- [1..10]]
-multiplesOfTwoComprehensionGreaterThanTwelve = [x*2 | x <- [1..10], x*2 >= 12]
+comprehensionMultiplesTwoGreaterThanTwelve = [x*2 | x <- [1..10], x*2 >= 12]
 comprehensionMod = [x | x <- [50.100], x `mod` 7 == 3]
