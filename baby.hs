bbd0f3539b0d389667ebd52e6ae212ccc5b09cbf
Stuart Gale <bishboria@gmail.com>
2012-05-24 19:23:35 +0100

    .

--- baby.hs
+++ baby.hs
@@ -4,7 +4,7 @@ doubleSmallNumber x = if x > 100
 			 then x
 			 else x*2
 
-doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+doubleSmallNumber' x = (if x > 100 then x else x*2)
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
 lostNumbers = [4,8,15,16,23,42]
