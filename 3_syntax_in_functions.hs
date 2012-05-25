5d538c7f0dba19496f025c192ca24f8613c20d47
Stuart Gale <bishboria@gmail.com>
2012-05-25 16:04:12 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -137,3 +137,8 @@ cylinder r h =
 
 
 theAnswer = 4 * (let a = 9 in a + 1) + 2
+letLocalFunction = [let square x = x*x in (square 5, square 3, square 2)]
+letMultiple = (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
+theAnswer' = (let (a,b,c) = (1,2,3) in a+b+c) * 100
+
+-- let in list comprehensions
