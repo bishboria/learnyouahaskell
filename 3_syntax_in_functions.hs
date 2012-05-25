22e3e804a496c494b655033b4b29852e0ba1dde4
Stuart Gale <bishboria@gmail.com>
2012-05-25 15:15:01 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -85,3 +85,20 @@ a `compare'` b
     | a == b = EQ
     | a <= b = LT
     | otherwise = GT
+
+bmiTell' :: Double -> Double -> String
+bmiTell' weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pfft, I bet you're ugly!"    	| bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+
+bmiTell'' :: Double -> Double -> String
+bmiTell'' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pfft, I bet you're ugly!"    	| bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+	  skinny = 18.5
+	  normal = 25.0
+	  fat = 30.0
