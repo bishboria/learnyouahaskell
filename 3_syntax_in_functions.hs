20436100edc284226589c8f96a6870ce0c299c3d
Stuart Gale <bishboria@gmail.com>
2012-05-25 15:22:41 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -102,3 +102,10 @@ bmiTell'' weight height
 	  skinny = 18.5
 	  normal = 25.0
 	  fat = 30.0
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan"
+greet "Fernando" = niceGreeting ++ " Fernando"
+greet name = badGreeting ++ " " ++ name
+    where niceGreeting = "Hello! So very nice to see you,"
+	  badGreeting = "Oh! Pfft. It's you."
