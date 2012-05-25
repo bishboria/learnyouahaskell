ffed36310afb64449484070d73cdf99180d7b4c0
Stuart Gale <bishboria@gmail.com>
2012-05-25 15:47:59 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -121,3 +121,19 @@ initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
     where (f:_) = firstname
           (l:_) = lastname -- better to do this in function params
 			   -- but illustrated to show possible
+
+-- functions in where blocks
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height^2
+
+
+-- let
+cylinder :: Double -> Double -> Double
+cylinder r h =
+	let sideArea = 2 * pi * r * h
+	    topArea = pi * r^2
+	in  sideArea + 2 * topArea
+
+
+theAnswer = 4 * (let a = 9 in a + 1) + 2
