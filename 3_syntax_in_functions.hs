f37e323938750a3299b8c1bdf6ecba2fa5a7c7e9
Stuart Gale <bishboria@gmail.com>
2012-05-25 13:52:45 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -1,3 +1,11 @@
 lucky :: Int -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe 5 = "Not between 1 and 5"
