38f715232eb6e0be5fbb010e3bab5295e8ca0a47
Stuart Gale <bishboria@gmail.com>
2012-05-25 14:35:39 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -47,3 +47,10 @@ summed = [a+b | (a,b) <- xs]
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
+
+
+tell :: Show a => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. First two elements are: " ++ show x ++ " and " ++ show y
