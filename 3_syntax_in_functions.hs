7aed1cf86061fb94c6e3beab8a68a3c90d5317a6
Stuart Gale <bishboria@gmail.com>
2012-05-25 14:14:55 +0100

    .

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -28,3 +28,12 @@ addVectors a b = (fst a + fst b, snd a + snd b)
 
 addVectors' :: (Double, Double) -> (Double, Double) -> (Double, Double)
 addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
