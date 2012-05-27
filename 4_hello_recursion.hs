b4ae2548ba330038f8d13a499f00a5ae32ec7c99
Stuart Gale <bishboria@gmail.com>
2012-05-27 13:28:58 +0100

    .

--- 4_hello_recursion.hs
+++ 4_hello_recursion.hs
@@ -2,3 +2,8 @@ maximum' :: Ord a => [a] -> a
 maximum' [] = error "maximum of empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x : replicate' (n - 1) x
