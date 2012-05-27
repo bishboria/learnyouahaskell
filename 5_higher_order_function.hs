2070125f89821be2e66a426eb72825f51c219c75
Stuart Gale <bishboria@gmail.com>
2012-05-27 16:49:41 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -43,3 +43,12 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+-- examples
+zipWith' (+) [4,2,5,6] [2,6,2,3]
+zipWith' max [6,3,2,1] [7,3,1,5]
+zipWith' (++) ["foo", "bar", "baz"] ["fighters", "hoppers", "aldrin"]
+zipWith' (*) (replicate 5 2) [1..]
+zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
