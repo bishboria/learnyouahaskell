66e5f28ef5639aa5a8b79aeef2b725d34b11cd13
Stuart Gale <bishboria@gmail.com>
2012-05-27 16:55:31 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -55,4 +55,5 @@ flip' f y x = f x y
 
 zip [1,2,3,4,5] "hello"
 flip' zip [1,2,3,4,5] "hello"
-zipWith' div [2,2..] [10,8,5,4,2]
+zipWith' div [2,2..] [10,8,6,4,2]
+zipWith' (flip' div) [2,2..] [10,8,6,4,2]
