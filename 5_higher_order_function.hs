5ce26be273153854dd6af241d5c6ce81e00deca5
Stuart Gale <bishboria@gmail.com>
2012-05-27 15:57:55 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -37,3 +37,9 @@ applyTwice (+3) 10
 applyTwice (++ " HAHA") "HEY"
 applyTwice ("HAHA " ++) "HEY"
 applyTwice (3:) [1]
+
+-- implementing zipWith
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
