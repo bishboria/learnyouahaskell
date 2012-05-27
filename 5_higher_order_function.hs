fb15362ea406da874aaf58bb0ecde721dd3ac311
Stuart Gale <bishboria@gmail.com>
2012-05-27 17:15:18 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -61,3 +61,9 @@ zipWith' (flip' div) [2,2..] [10,8,6,4,2]
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
 map' f (x:xs) f x : map' f xs
+-- examples
+map (+3) [1,5,3,1,6]
+map (++ "!") ["BIFF", "BANG", "POW"]
+map (replicate 3) [3..6]
+map (map (^2)) [[1,2],[3,4,5,6],[7,8]]
+map fst [(1,3),(3,5),(6,3),(2,6),(2,5)]
