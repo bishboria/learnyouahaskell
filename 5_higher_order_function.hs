a6419e8e0565383a25e342dadda0cac1f3bf3ed0
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:14:03 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -99,3 +99,4 @@ largestDivisible = head (filter' p [100000,99999..])
 takeWhile (/=' ') "elephants know how to party"
 
 sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+sum (takeWhile (<10000) [m | m <- [n^2 | n <- [1..]], odd m])
