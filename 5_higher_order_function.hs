c5144a1a03861d69b95a027eab812a9ae5e79f75
Stuart Gale <bishboria@gmail.com>
2012-05-28 15:30:43 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -106,3 +106,7 @@ chain 1 = [1]
 chain n
     | even n = n : chain (n `div` 2)
     | odd n = n : chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
