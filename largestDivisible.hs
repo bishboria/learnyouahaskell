c1d8d2df49b7347c1448665a32c840afb141f849
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:32:09 +0100

    Accidently added scratch files into repo.

--- largestDivisible.hs
+++
@@ -1,5 +0,0 @@
-chain :: Integer -> [Integer]
-chain 1 = [1]
-chain n
-    | even n = n : chain (n `div` 2)
-    | odd n = n : chain (n*3 + 1)
--- take.hs
+++
@@ -1,5 +0,0 @@
-take' :: (Num i, Ord i) => i -> [a] -> [a]
-take' n _
-    | n <= 0 = []
-take' _ [] = []
-take' n (x:xs) = x : take' (n - 1) xs
