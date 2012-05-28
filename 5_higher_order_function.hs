8eed646ad62d3df668c4a99dd09cc20bd5361d31
Stuart Gale <bishboria@gmail.com>
2012-05-28 17:38:01 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -168,3 +168,6 @@ reverse'' = foldl (flip (:)) []
 
 product' :: Num a => [a] -> a
 product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
