d8251b2277d6b65ef01782e7f3659ea87bd1aaab
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:22:26 +0100

    .

--- largestDivisible.hs
+++ largestDivisible.hs
@@ -1,3 +1,5 @@
-largestDivisible :: Integer
-largestDivisible = head (filter p [100000,99999..])
-    where p x = x `mod` 3829 == 0
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n = n : chain (n*3 + 1)
