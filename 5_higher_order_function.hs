3fe1f09fa669f066784ae7ccc4a7c289a7be5b4b
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:03:01 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -82,3 +82,5 @@ filter' (`elem` ['a'..'z']) "u LaUgH aT mE BeCaUsE I aM diFfeRent"
 filter' (`elem` ['A'..'Z']) "i LAuGh at you bEcause u R all the same"
 
 filter' (<15) (filter even [1..20])
+-- equivalent to
+[x | x <- [1..20], x < 15, even x]
