97f57086a3b0dbfc5738da804387efc634399a51
Stuart Gale <bishboria@gmail.com>
2012-05-28 16:22:22 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -157,4 +157,4 @@ map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 -- so foldr is usually used to build new lists from a list
 
 elem' :: (Eq a) => a -> [a] -> Bool
-elem' y ys = foldr (\x acc -> if x = y then True else acc) False ys
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
