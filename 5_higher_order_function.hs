f25b6255ea65e0844d7b81358f97c805a164fa36
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:06:09 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -89,5 +89,7 @@ quicksort' :: Ord a => [a] -> [a]
 quicksort' [] = []
 quicksort' (x:xs) = 
     let smallerOrEqual = filter' (<= x) xs
-	larger = filter (> x) xs
+	larger = filter' (> x) xs
     in  quicksort' smallerOrEqual ++ [x] ++ quicksort larger
+
+
