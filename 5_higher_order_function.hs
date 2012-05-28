57695450ba4fe000a82552cae0cd965869cc49b0
Stuart Gale <bishboria@gmail.com>
2012-05-28 15:39:15 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -122,3 +122,9 @@ map (\x -> x + 3) [1,6,3,2]
 zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5]
 
 map (\(a,b) -> a + b) [(1,2),(3,5),(6,3),(2,6),(2,5)]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+-- is equivalent to
+addThree' :: Int -> Int -> Int -> Int
+addThree' = \x -> \y -> \z -> x + y + z
