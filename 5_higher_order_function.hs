944143c0b5be4513b6e586189ce7d36eb1a7a108
Stuart Gale <bishboria@gmail.com>
2012-05-28 14:06:16 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -90,6 +90,6 @@ quicksort' [] = []
 quicksort' (x:xs) = 
     let smallerOrEqual = filter' (<= x) xs
 	larger = filter' (> x) xs
-    in  quicksort' smallerOrEqual ++ [x] ++ quicksort larger
+    in  quicksort' smallerOrEqual ++ [x] ++ quicksort' larger
 
 
