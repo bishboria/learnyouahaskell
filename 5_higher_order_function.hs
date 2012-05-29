5925b2939a295e721b36ca22c637641b037518ea
Stuart Gale <bishboria@gmail.com>
2012-05-29 12:40:17 +0100

    .

--- 5_higher_order_function.hs
+++ 5_higher_order_function.hs
@@ -240,3 +240,6 @@ map (negate . sum . tail) [[1..5],[3..6],[1..7]]
 sum (replicate 5 (max 6.7 8.9))
 (sum . replicate 5) (max 6.7 8.9)
 sum . replicate 5 $ max 6.7 8.9
+
+replicate 2 (product (map (*3) (zipWith max [1,2] [4,5])))
+replicate 2 . product . map (*3) $ zipWith max [1,2] [4,5]
