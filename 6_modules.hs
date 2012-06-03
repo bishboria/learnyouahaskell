f60b388424d28f6a7986fd00fb8f55be0a5e852a
Stuart Gale <bishboria@gmail.com>
2012-06-03 13:48:41 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -2,3 +2,8 @@ import Data.List
 
 numUniques :: Eq a => [a] -> Int
 numUniques = length . nub
+
+-- import Data.List (nub, sort)
+-- import Data.List hiding (nub)
+-- import qualified Data.Map
+-- import qualified Data.Map as M
