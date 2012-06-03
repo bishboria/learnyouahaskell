ab58b1264b8481f3883185f58211d59ee1360b48
Stuart Gale <bishboria@gmail.com>
2012-06-03 16:27:40 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -87,3 +87,7 @@ findKey key [] = Nothing
 findKey key ((k,v):xs)
     | key == k = Just v
     | otherwise = findKey key xs
+
+findKeyFold :: Eq k => k -> [(k,v)] -> Maybe v
+findKeyFold key xs = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing xs
+-- findKeyFold is the same as Data.List.lookup
