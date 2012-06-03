1d98ea44831b2a1720ad0f42d833347272065d0c
Stuart Gale <bishboria@gmail.com>
2012-06-03 15:15:58 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -70,3 +70,17 @@ firstTo40 = find (\x -> digitSum x == 40) [1..]
 
 firstTo :: Int -> Maybe Int
 firstTo n = find (\x -> digitSum x == n) [1..]
+
+
+-- Mapping Keys to Values
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+findKey :: Eq k => k -> [(k,v)] -> v
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
