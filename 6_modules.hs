721e9774c54f49ec880de3c6df06e1a8c8b82418
Stuart Gale <bishboria@gmail.com>
2012-06-03 14:13:34 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -41,3 +41,6 @@ mo = map ord "abcdefgh"
 
 encode :: Int -> String -> String
 encode offset msg = map (chr . (+ offset) . ord) msg -- (\c -> chr $ ord c + offset)
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
