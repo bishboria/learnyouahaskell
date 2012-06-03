e79327bd69b026f6a0d0c95d8ef10e05f7daea34
Stuart Gale <bishboria@gmail.com>
2012-06-03 13:55:56 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -14,3 +14,5 @@ g = group [1,1,1,1,1,1,1,1,1,2,2,3,2,2,5,6,7]
 g' = group ["boom","bip","bip","boom","boom"]
 s = sort [5,4,3,7,2,1]
 s' = sort ["boom","bip","bip","boom","boom"]
+
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
