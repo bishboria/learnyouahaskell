26097e6ad899c44bfdaf8694144c706528e0b9a4
Stuart Gale <bishboria@gmail.com>
2012-06-03 16:20:07 +0200

    .

--- 6_modules.hs
+++ 6_modules.hs
@@ -82,5 +82,8 @@ phoneBook =
     ,("penny", "853-2492")
     ]
 
-findKey :: Eq k => k -> [(k,v)] -> v
-findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+findKey :: Eq k => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs)
+    | key == x = Just v
+    | otherwise = findKey key xs
