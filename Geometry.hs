ebde53c2b69dbca50949a7005aa5ca7b1073a936
Stuart Gale <bishboria@gmail.com>
2012-06-05 21:47:24 +0100

    .

---
+++ Geometry.hs
@@ -0,0 +1,29 @@
+module Geometry
+( areaCube
+, areaCuboid
+, areaSphere
+, volumeCube
+, volumeCuboid
+, volumeSphere
+) where
+
+areaCube :: Float -> Float
+areaCube side = areaCuboid side side side
+
+areaCuboid :: Float -> Float -> Float -> Float
+areaCuboid a b c = areaRect a b * 2 + areaRect a c * 2 + areaRect c b * 2
+
+areaRect :: Float -> Float -> Float
+areaRect a b = a * b
+
+areaSphere :: Float -> Float
+areaSphere radius = 4 * pi * (radius ^ 2)
+
+volumeCube :: Float -> Float
+volumeCube side = volumeCuboid side side side
+
+volumeCuboid :: Float -> Float -> Float -> Float
+volumeCuboid a b c = areaRect a b * c
+
+volumeSphere :: Float -> Float
+volumeSphere radius = (4.0 / 3.0) * pi * (radius ^ 3)
