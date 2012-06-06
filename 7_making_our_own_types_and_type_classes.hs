798022f327cc0ecf8c1c5a02f31e5c113cc85048
Stuart Gale <bishboria@gmail.com>
2012-06-06 22:35:50 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -1,9 +1,10 @@
 data Bool' = False | True
 
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-concentricCircles = map (Circle 10 20) [4,5,6,6]
+concentricCircles = map (Circle (Point 0 0)) [4,5,6,6]
