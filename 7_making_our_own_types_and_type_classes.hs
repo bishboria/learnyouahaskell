93d5b3d5fa7ebd3dbe83ba6a9838325430821378
Stuart Gale <bishboria@gmail.com>
2012-06-06 22:29:15 +0100

    .

---
+++ 7_making_our_own_types_and_type_classes.hs
@@ -0,0 +1,7 @@
+data Bool' = False | True
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
