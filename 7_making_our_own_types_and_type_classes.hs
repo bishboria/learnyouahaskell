75aa28730bec02dc47baa57e420a438b448ce159
Stuart Gale <bishboria@gmail.com>
2012-06-08 16:22:39 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -34,4 +34,23 @@ nudgedRectangle = nudge (baseRect 40 100) 60 23
 concentricCircles = map (baseCircle) [4,5,6,6]
 
 data Person = Person String String Int Float String String deriving (Show)
+
+firstName :: Person -> String
+firstName (Person firstname _ _ _ _ _) = firstname
+
+lastName :: Person -> String
+lastName (Person _ lastname _ _ _ _) = lastname
+
+age :: Person -> Int
+age (Person _ _ age _ _ _) = age
+
+height :: Person -> Float
+height (Person _ _ _ height _ _) = height
+
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ number _) = number
+
+flavour :: Person -> String
+flavour (Person _ _ _ _ _ flavour) = flavour
+
 guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
