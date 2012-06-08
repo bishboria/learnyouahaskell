a7ec4681d2eb438a4b0a071fa916a99782b9c87e
Stuart Gale <bishboria@gmail.com>
2012-06-08 15:18:16 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -14,7 +14,8 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
-area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Rectangle (Point x1 y1) (Point x2 y2)) =
+	(abs $ x2 - x1) * (abs $ y2 - y1)
 
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
