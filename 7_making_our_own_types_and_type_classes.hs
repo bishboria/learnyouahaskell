0b8c9f111065483ab9d7aeddaf675c94ec0277dd
Stuart Gale <bishboria@gmail.com>
2012-06-11 16:54:31 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -244,6 +244,7 @@ assocListIntInt = [(1,2),(3,5),(8,9)] :: AssocList Int Int
 -- of Haskell. This includes data & type declarations as well as after ::
 -- in type declarations or type annotations
 
+
 -- Go Left, Then Right
 data Either' a b = Left' a | Right' b deriving (Eq, Ord, Read, Show)
 right'20 = Right' 20
