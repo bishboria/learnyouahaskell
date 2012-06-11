d3875175cb623eab755c62a0cc544acbaca59d97
Stuart Gale <bishboria@gmail.com>
2012-06-11 15:34:30 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -176,3 +176,7 @@ just100GtJust50 = Just 100 > Just 50
 -- Any Day of the Week
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 	   deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+wednesday = Wednesday
+showWednesday = show Wednesday
+readSaturday = read "Saturday" :: Day
