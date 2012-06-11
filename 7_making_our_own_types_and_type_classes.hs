20cad1e42d7c0ab4d163666832dd7e06987ad92c
Stuart Gale <bishboria@gmail.com>
2012-06-11 16:52:25 +0100

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -202,3 +202,13 @@ minMaxRangeDay = [minBound .. maxBound] :: [Day]
 -- the following are exactly the same
 -- toUpperString :: [Char] -> [Char]
 -- toUpperString :: String -> String
+
+phoneBook :: [(String, String)]
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
