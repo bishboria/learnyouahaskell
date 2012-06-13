3940dcf3e65d820c879788444815998784b01858
Stuart Gale <bishboria@gmail.com>
2012-06-13 16:01:25 +0300

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -259,3 +259,11 @@ left'woot = Left' "w00t"
 data LockerState = Taken | Free deriving (Show, Eq)
 type Code = String
 type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker " ++ show lockerNumber ++ " doesn't exist!"
+    Just (state, code) -> if state /= Taken
+			     then Right code
+			     else Left $ "Locker " ++ show lockerNumber
+                                         ++ " is already taken!"
