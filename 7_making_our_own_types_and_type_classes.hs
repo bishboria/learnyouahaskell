761a6d41307325497a7c000afdef30d73bc46dd5
Stuart Gale <bishboria@gmail.com>
2012-06-14 21:54:17 +0300

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -326,3 +326,10 @@ treeInsert x (Node a left right)
     | x == a = Node x left right
     | x < a = Node a (treeInsert x left) right
     | x > a = Node a left (treeInsert x right)
+
+treeElem :: Ord a => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
