325c118e082b1416aa63fd85e6e85edc9b422dae
Stuart Gale <bishboria@gmail.com>
2012-06-14 21:39:08 +0300

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -319,3 +319,10 @@ data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
 
 singleton :: a -> Tree a
 singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: Ord a => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
