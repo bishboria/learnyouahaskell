747be66dd62cb73dcf75212e268fe9193ab9e431
Stuart Gale <bishboria@gmail.com>
2012-06-14 21:29:53 +0300

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -316,3 +316,6 @@ listThree = listOne ^++ listTwo
 
 -- Let's Plant a Tree
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
