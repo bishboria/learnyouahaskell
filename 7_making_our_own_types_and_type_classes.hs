ab2f8b468c88919a532cfd8e2dab6e8a6996c0f1
Stuart Gale <bishboria@gmail.com>
2012-06-15 10:03:46 +0300

    .

--- 7_making_our_own_types_and_type_classes.hs
+++ 7_making_our_own_types_and_type_classes.hs
@@ -418,3 +418,7 @@ instance Eq m => Eq (Maybe m) where  -- this definition guarantees m
 
 class YesNo a where
     yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
