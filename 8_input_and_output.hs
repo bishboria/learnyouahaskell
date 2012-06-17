9dc83a02e5b4daad944a0cea0c286719d4e1f2e1
Stuart Gale <bishboria@gmail.com>
2012-06-18 01:59:27 +0300

    .

--- 8_input_and_output.hs
+++ 8_input_and_output.hs
@@ -8,3 +8,12 @@ putStrLn :: String -> IO ()
 :t putStrLn "hello, world"
 putStrLn "hello, wolrd" :: IO ()
 -}
+
+
+-- Gluing IO actions together
+{-
+main = do
+    putStrLn "Hello, what's your name?"
+    name <- getLine
+    putStrLn ("Hey " ++ name ++ ", you rock!"
+-}
