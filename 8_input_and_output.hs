7b8e7cc5ab0a97864e7a85bd7cce7d31f71928e3
Stuart Gale <bishboria@gmail.com>
2012-06-25 12:34:11 +0100

    .

--- 8_input_and_output.hs
+++ 8_input_and_output.hs
@@ -17,3 +17,24 @@ main = do
     name <- getLine
     putStrLn ("Hey " ++ name ++ ", you rock!"
 -}
+
+-- getLine returns an IO String
+-- to get at the string use <-
+
+-- you cannot do
+{-
+nameTag = "Hello, my name is " ++ getLine
+-}
+-- ++ expects two lists. Here LHS is [Char]/String
+-- RHS is IO String.
+
+-- The last action in a do block cannot be bound to a name
+-- This is because of monadic reasons.
+
+-- Except for the last line, every line in a do block can be written with
+-- a bind. But it's missed out for unimportant things like putStrLn.
+
+{-
+myLine = getLine
+-}
+-- all this does is give getLine a new name. myLine is still an IO action
