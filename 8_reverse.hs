b3dbc0286b7992d683de62c37e46a13bd48f3eba
Stuart Gale <bishboria@gmail.com>
2012-06-25 13:01:00 +0100

    .

--- 8_reverse.hs
+++ 8_reverse.hs
@@ -8,3 +8,9 @@ main = do
 
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+-- recursive function is ok because main is an IO action
+-- and the do block inside the else wraps two IO actions into one
+
+-- return in haskell does not exit a function. It wraps a pure value in an
+-- IO action. Monad stuff again
