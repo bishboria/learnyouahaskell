967feb5dc4e769538cae02e1442272f3b9f344e2
Stuart Gale <bishboria@gmail.com>
2012-07-10 22:49:33 +0100

    .

---
+++ 09/9_deleteTodo_brackOnError.hs
@@ -0,0 +1,24 @@
+import System.IO
+import System.Directory
+import Data.List
+import Control.Exception
+
+main = do
+    contents <- readFile "9_todo.txt"
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    putStrLn "Which one do you want to delete?"
+    numberString <- getLine
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "9_todo.txt"
+            renameFile tempName "9_todo.txt")
--- 09/9_more_input_and_more_output.hs
+++ 09/9_more_input_and_more_output.hs
@@ -159,3 +159,38 @@ main = do
     removeFile "9_todo.txt"
     renameFile tempName "9_todo.txt"
 -- run ./9_deleteTodo
+
+
+-- Cleaning up
+
+-- if the program errors after the temp file is openend then the temp file
+-- does not get removed.
+-- Fix this by using bracketOnError from Control.Exception
+-- bracketOnError only performs the cleanup if an exception is raised.
+-- bracket always performs the cleanup
+
+import System.IO
+import System.Directory
+import Data.List
+import Control.Exception
+
+main = do
+    contents <- readFile "9_todo.txt"
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    putStrLn "Which one do you want to delete?"
+    numberString <- getLine
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "9_todo.txt"
+            renameFile tempName "9_todo.txt")
+-- run ./9_deleteTodo_bracketOnError
