4bf1fa13b059320669eeb966730b3c542d612327
Stuart Gale <bishboria@gmail.com>
2012-07-14 15:57:47 +0100

    The book has broken code! HATE THAT

--- 09/9_more_input_and_more_output.hs
+++ 09/9_more_input_and_more_output.hs
@@ -216,3 +216,56 @@ main = do
     mapM putStrLn args
     putStrLn "The program name is:"
     putStrLn progName
+
+
+-- More Fun with To-Do Lists
+
+-- implementing an entire todo that takes user input and dispatches
+-- based on the values matched.
+
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "9_todo.txt"
+            renameFile tempName "9_todo.txt")
+-- In the book removeFile "todo.txt" & renameFile... are still hardcoded!
+-- Tut. Tut. Tut.
---
+++ 09/9_todo.hs
@@ -0,0 +1,46 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "9_todo.txt"
+            renameFile tempName "9_todo.txt")
+-- In the book removeFile "todo.txt" & renameFile... are still hardcoded!
+-- Tut. Tut. Tut.
