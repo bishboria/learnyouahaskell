c541a73596d9a97c1a6be84614f524681cd0dc4e
Stuart Gale <bishboria@gmail.com>
2012-07-28 17:48:51 +0100

    .

--- 10/10_functionally_solving_problems.hs
+++ 10/10_functionally_solving_problems.hs
@@ -35,3 +35,52 @@ myFl' f z (x:xs) = let z' = f z x
                    in seq z' $ myFl' f z' xs -- apply z' now and then
                                              -- return myFl' f z' xs
 -- /Reminder
+
+
+-- Quickest Path (Heathrow to London...)
+-- Book does a terrible job explaining it!
+
+data Section = Section { getA :: Int, getB :: Int, getC :: Int }
+    deriving (Show)
+-- could have used (Int,Int,Int) But then they could also be vectors...
+-- declaring the type, instead, means we can't accidently add a vector to a
+-- section of a road system.
+
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon  = [ Section 50 10 30
+                    , Section 5 90 20
+                    , Section 40 2 25
+                    , Section 10 8 0
+                    ]
+
+data Label = A | B | C deriving (Show)
+type Path  = [(Label,Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let timeA = sum (map snd pathA)
+        timeB = sum (map snd pathB)
+        forwardTimeToA = timeA + a
+        crossTimeToA = timeB + b + c
+        forwardTimeToB = timeB + b
+        crossTimeToB = timeA + a + c
+        newPathToA = if forwardTimeToA <= crossTimeToA
+                        then (A, a):pathA
+                        else (C, c):(B, b):pathB
+        newPathToB = if forwardTimeToB <= crossTimeToB
+                        then (B, b):pathB
+                        else (C, c):(A, a):pathA
+    in  (newPathToA, newPathToB)
+-- The paths are the wrong way round as it's faster to prepend a list...
+-- If we took and returned timeA and timeB, we wouldn't need to do
+--      sum (map snd pathX)
+-- everytime
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+            then reverse bestAPath
+            else reverse bestBPath
