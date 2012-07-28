6d1c3b5906dc0d500c4ba031931302bf6732cd2a
Stuart Gale <bishboria@gmail.com>
2012-07-28 16:55:06 +0100

    .

--- 10/10_functionally_solving_problems.hs
+++ 10/10_functionally_solving_problems.hs
@@ -12,3 +12,18 @@ solveRPN  = head . foldl foldingFunction [] . words
 -- solveRPN "90 34 12 33 55 66 + * - +"
 -- solveRPN "90 34 12 33 55 66 + * - + -"
 -- solveRPN "90 3.8 -"
+
+-- Reminder
+-- comments assume f needs to evaluate both operands before it can return
+myFr f z [] = z
+myFr f z (x:xs) = f x $ myFr f z xs -- aplies f x $ ... when end of list reached
+
+myFl f z [] = z
+myFl f z (x:xs) = myFl f (f z x) xs -- applies (f z x) lazily so doesn't reduce
+                                    -- straight away
+
+myFl' f z [] = z
+myFl' f z (x:xs) = let z' = f z x
+                   in seq z' $ myFl' f z' xs -- apply z' now and then
+                                             -- return myFl' f z' xs
+-- /Reminder
