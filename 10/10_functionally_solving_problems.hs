cd26e67c34dfd275533dfc1119cae722a4c09fe1
Stuart Gale <bishboria@gmail.com>
2012-07-28 16:57:28 +0100

    .

--- 10/10_functionally_solving_problems.hs
+++ 10/10_functionally_solving_problems.hs
@@ -2,9 +2,13 @@
 
 solveRPN :: String -> Double
 solveRPN  = head . foldl foldingFunction [] . words
-    where   foldingFunction (x:y:ys) "*"    = (y * x):ys
-            foldingFunction (x:y:ys) "+"    = (y + x):ys
-            foldingFunction (x:y:ys) "-"    = (y - x):ys
+    where   foldingFunction (x:y:ys) "*"    = (y * x) :ys
+            foldingFunction (x:y:ys) "+"    = (y + x) :ys
+            foldingFunction (x:y:ys) "-"    = (y - x) :ys
+            foldingFunction (x:y:ys) "/"    = (y / x) :ys
+            foldingFunction (x:y:ys) "^"    = (y ** x):ys
+            foldingFunction (x:xs)   "ln"   = log x   :xs
+            foldingFunction xs "sum"        = [sum xs]
             foldingFunction xs numberString = read numberString:xs
 
 -- solveRPN "10 4 3 + 2 * -"
