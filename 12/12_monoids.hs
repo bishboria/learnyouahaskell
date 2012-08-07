283ad48579a5ad4536c902cc580f0b4ae8eee3fc
Stuart Gale <bishboria@gmail.com>
2012-08-07 19:58:16 +0300

    .

--- 12/12_monoids.hs
+++ 12/12_monoids.hs
@@ -338,3 +338,14 @@ lengthCompare x y = (length x `compare` length y) `mappend`
                     (vowels x `compare` vowels y) `mappend`
                     (x `compare` y)
     where vowels  = length . filter (`elem` "aeiou")
+
+lengthCompare "zen" "anna"
+-- LT
+lengthCompare "zen" "ana"
+-- LT
+lengthCompare "zen" "ann"
+-- GT
+
+-- The Ordering monoid allows us to easily compare things by many different
+-- criteria and put those criteria in an order themselves, ranging from
+-- most important to least.
