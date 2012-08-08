b09d6b668aaf95092e548baa54c068bf09f8b472
Stuart Gale <bishboria@gmail.com>
2012-08-08 20:57:47 +0300

    .

--- 13/13_tightrope.hs
+++ 13/13_tightrope.hs
@@ -10,3 +10,6 @@ landRight :: Birds -> Pole -> Maybe Pole
 landRight n (left, right)
     | abs (left - (right + n)) < 4 = Just (left, right + n)
     | otherwise                    = Nothing
+
+walked = return (0,0) >>= landLeft 2 >>= landRight 1 >>= landLeft 2 >>= landRight (-1)
+-- walked == Nothing
